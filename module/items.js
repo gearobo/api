921f0b16edd52bb999a4d2fd97d264cc405d57d6
Gearobo <developersgear@gmail.com>
2023-08-13 17:37:19 +0530

    final commit

--- module/items.js
+++ module/items.js
@@ -1,13 +1,18 @@
-const mongoose =require("mongoose");
+const mongoose = require( "mongoose");
 
 const itemSchema = mongoose.Schema({
-    name: { type: String },
-    number: {
-      type: String
+    name: { 
+      type: String, 
+      // required: true 
     },
-
-    
-    comment: { type: String },
+    number: {
+      type: String,
+      // required: true
+    },
+    comment: { 
+      type: String,
+      // required: true
+    },
   });
 
   module.exports=mongoose.model("Item",itemSchema);
--- routes/items.js
+++ routes/items.js
@@ -1,8 +1,9 @@
 const express =require("express");
 const router =express.Router();
-
+const app = express();
 const {postAllItems} =require("../controller/items");
-
+//const bodyParser = require("body-parser");
+//app.use(bodyParser.urlencoded({ extended: false }));
 router.route("/items").post(postAllItems);
 
 module.exports=router;
